6b3b890fa007805f64458ff766997c3b6b17f577
Monika-Lis <144853392+Monika-Lis@users.noreply.github.com>
2024-02-13 12:04:31 +0000

    Deploying to gh-pages from @ Monika-Lis/goit-js-hw-09@8108335af745dfdf0ab093f973aad489d5ea4ba7 🚀

---
+++ 01-color-switcher.4bf0aed8.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");let r=null;e.addEventListener("click",(()=>{r=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16)}`;t.style.backgroundColor=e,localStorage.setItem("backgroundColor",e)}),1e3),e.disabled=!0})),o.addEventListener("click",(()=>{clearInterval(r);localStorage.getItem("backgroundColor");e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.4bf0aed8.js.map
