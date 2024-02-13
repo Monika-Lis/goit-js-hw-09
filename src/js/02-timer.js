77140ed7e61d9ceb1d3f27b07d4f1eb18204ea0b
Monika-Lis <lis.monikak@gmail.com>
2024-02-13 23:00:12 +0000

    timer

---
+++ src/js/02-timer.js
@@ -0,0 +1,81 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const datetimePicker = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
+const daysSpan = document.querySelector('span[data-days]');
+const hoursSpan = document.querySelector('span[data-hours]');
+const minutesSpan = document.querySelector('span[data-minutes]');
+const secondsSpan = document.querySelector('span[data-seconds]');
+
+startBtn.disabled = true;
+let selectedDate = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const currentDate = new Date();
+    if (selectedDates[0] <= currentDate) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+    } else {
+      startBtn.disabled = false;
+      selectedDate = selectedDates[0];
+    }
+  },
+};
+
+flatpickr(datetimePicker, options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function updateTimer({ days, hours, minutes, seconds }) {
+  daysSpan.textContent = addLeadingZero(days);
+  hoursSpan.textContent = addLeadingZero(hours);
+  minutesSpan.textContent = addLeadingZero(minutes);
+  secondsSpan.textContent = addLeadingZero(seconds);
+}
+
+function startCountdown() {
+  const intervalTime = setInterval(() => {
+    const currentTime = Date.now();
+    const remainingTime = selectedDate - currentTime;
+    const readTime = convertMs(remainingTime);
+
+    updateTimer(readTime);
+
+    if (remainingTime <= 1000) {
+      clearInterval(intervalTime);
+      startBtn.disabled = false;
+    }
+  }, 1000);
+}
+
+startBtn.addEventListener('click', () => {
+  startCountdown();
+  startBtn.disabled = true;
+});
