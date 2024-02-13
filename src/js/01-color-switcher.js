8108335af745dfdf0ab093f973aad489d5ea4ba7
Monika-Lis <lis.monikak@gmail.com>
2024-02-13 12:04:10 +0000

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const websiteBody = document.querySelector('body');
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+let timerId = null;
+
+buttonStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    const newColor = getRandomHexColor();
+    websiteBody.style.backgroundColor = newColor;
+    localStorage.setItem('backgroundColor', newColor);
+  }, 1000);
+  buttonStart.disabled = true;
+});
+
+buttonStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  const savedColor = localStorage.getItem('backgroundColor');
+  buttonStart.disabled = false;
+});
